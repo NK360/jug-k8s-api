33ae8a206c3070e4e2fe2992973e8320559fbb4c
Jochen Niebuhr <jniebuhr@cfire.de>
2017-07-19 14:55:24 +0200

    Add prometheus metrics

--- src/main/java/de/nk360/jug/JugApiApplication.java
+++ src/main/java/de/nk360/jug/JugApiApplication.java
@@ -3,7 +3,12 @@ package de.nk360.jug;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+
 @SpringBootApplication
+@EnableSpringBootMetricsCollector
+@EnablePrometheusEndpoint
 public class JugApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JugApiApplication.class, args);
