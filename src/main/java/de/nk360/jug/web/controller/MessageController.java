cfcbe143284bf0e2a18fcbf2b2a1937e33286402
Jochen Niebuhr <jniebuhr@cfire.de>
2017-07-19 15:13:54 +0200

    Add configurable message

--- src/main/java/de/nk360/jug/web/controller/MessageController.java
+++ src/main/java/de/nk360/jug/web/controller/MessageController.java
@@ -4,6 +4,7 @@ import de.nk360.jug.web.model.MessageResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,12 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("message")
+@ConfigurationProperties("application.messages")
 public class MessageController {
     private static Logger logger = LoggerFactory.getLogger(MessageController.class);
 
+    private String message = "Hello %s!";
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<MessageResponse> actionGet(@RequestParam(name = "name") String name) {
-        logger.info("Sent message 'Hello " + name + "!'.");
-        return ResponseEntity.ok(new MessageResponse("Hello " + name + "!"));
+        String templatedMessage = String.format(message, name);
+        logger.info("Sent message '" + templatedMessage + "'.");
+        return ResponseEntity.ok(new MessageResponse(templatedMessage));
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public MessageController setMessage(String message) {
+        this.message = message;
+        return this;
     }
 }
