5fbb4996bce793721e9992752213a889f96348cd
Tobias Kleine <tobias.kleine@nk360.de>
2017-07-20 17:55:01 +0200

    add logger

--- src/main/java/de/nk360/jugapi/web/controller/MessageController.java
+++ src/main/java/de/nk360/jugapi/web/controller/MessageController.java
@@ -2,6 +2,8 @@ package de.nk360.jugapi.web.controller;
 
 import de.nk360.jugapi.web.model.MessageResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,11 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("message")
 public class MessageController {
+    private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
     private String message = "Hello %s!";
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<MessageResponse> actionGet(@RequestParam(name = "name") String name) {
         String templatedMessage = String.format(message, name);
+        logger.info("Sent message '" + templatedMessage + "'.");
         return ResponseEntity.ok(new MessageResponse(templatedMessage));
     }
 }
