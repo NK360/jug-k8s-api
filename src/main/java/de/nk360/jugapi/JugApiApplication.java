3dd5d15db3109b86638ff2cbcaa4f1b5ad50235b
Tobias Kleine <tobias.kleine@nk360.de>
2017-07-20 07:30:20 +0200

    initial commit, start.spring.io

---
+++ src/main/java/de/nk360/jugapi/JugApiApplication.java
@@ -0,0 +1,12 @@
+package de.nk360.jugapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugApiApplication.class, args);
+	}
+}
